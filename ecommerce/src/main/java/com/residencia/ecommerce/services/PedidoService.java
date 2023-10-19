c3138a8c0d102a8c1ce1520c8e78a4545705cf70
Felipe Sutter <lipeps61@gmail.com>
2023-10-19 17:02:07 -0300

    feat: criação do dtos

--- ecommerce/src/main/java/com/residencia/ecommerce/services/PedidoService.java
+++ ecommerce/src/main/java/com/residencia/ecommerce/services/PedidoService.java
@@ -1,10 +1,14 @@
 package com.residencia.ecommerce.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.residencia.ecommerce.dto.RelatorioPedidoDTO;
+import com.residencia.ecommerce.entities.ItemPedido;
 import com.residencia.ecommerce.entities.Pedido;
 import com.residencia.ecommerce.repositories.PedidoRepository;
 
@@ -13,6 +17,8 @@ public class PedidoService {
 
 	@Autowired
 	PedidoRepository pedidoRepo;
+	
+	private ModelMapper modelMapper = new ModelMapper();
 
 	public List<Pedido> listarPedidos() {
 		return pedidoRepo.findAll();
@@ -48,6 +54,31 @@ public class PedidoService {
 		if (pedidoContinuaExistindo == null)
 			return true;
 		return false;
+	}
+	
+	private RelatorioPedidoDTO entityToDto(Pedido pedido) {
+		RelatorioPedidoDTO relatorioPedidoDTO = modelMapper.map(pedido, RelatorioPedidoDTO.class);
+		
+		List<ItemPedido> itensPedidos = new ArrayList<>();
+		
+		for(ItemPedido itemPedido : pedido.getItemPedidos()) {
+			itensPedidos.add(itemPedido);
+		}
+	
+		relatorioPedidoDTO.setDataPedido(pedido.getDataPedido());
+		relatorioPedidoDTO.setIdPedido(pedido.getIdPedido());
+		relatorioPedidoDTO.setItemPedidos(itensPedidos);
+		return relatorioPedidoDTO;
+	}
+	
+	private Pedido DtoToEntity(RelatorioPedidoDTO relatorioPedidoDTO) {
+		Pedido pedido = modelMapper.map(relatorioPedidoDTO, Pedido.class);
+		return pedido;
+	}
+	
+	public RelatorioPedidoDTO criarRelatorioDTO(RelatorioPedidoDTO pedidoDTO) {
+		Pedido pedido = DtoToEntity(pedidoDTO);
+		return entityToDto(pedidoRepo.save(pedido));
 	}
 
 }
