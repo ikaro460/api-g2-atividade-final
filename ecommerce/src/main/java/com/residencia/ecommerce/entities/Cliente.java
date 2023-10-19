eb45a6510cfb1b2ef56fc7b4564134cfd98b2249
Beatriz Barcelos <bcardozo@faeterj-petropolis.edu.br>
2023-10-19 15:02:59 -0300

    entidade cliente

---
+++ ecommerce/src/main/java/com/residencia/ecommerce/entities/Cliente.java
@@ -0,0 +1,104 @@
+package com.residencia.ecommerce.entities;
+
+import java.sql.Date;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idCliente", scope = Cliente.class)
+@Entity
+@Table(name = "cliente")
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
+	private Long idCliente;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "nome_completo")
+	private String nomeCompleto;
+	
+	@Column(name = "cpf")
+	private String cpf;
+	
+	@Column(name = "telefone")
+	private String telefone;
+	
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
+	
+	@OneToOne
+	@JoinColumn(name = "id_endereco", referencedColumnName = "id_endereco", unique = true) 
+	private Endereco endereco;
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
+}
+
